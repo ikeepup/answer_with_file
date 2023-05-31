33744b8ec3a9fed8e5e317e6f185f3ce50d53624
Alex <405339683@qq.com>
2023-05-31 17:16:09 +0800

    fix: remove size limit

--- apps/file-q-and-a/nextjs/src/pages/api/process-file.ts
+++ apps/file-q-and-a/nextjs/src/pages/api/process-file.ts
@@ -1,67 +1,64 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import formidable, { Fields, Files } from "formidable"; // to handle file uploads
+import type { NextApiRequest, NextApiResponse } from 'next'
+import formidable, { Fields, Files } from 'formidable' // to handle file uploads
 
-import { TextEmbedding } from "../../types/file";
-import extractTextFromFile from "../../services/extractTextFromFile";
-import { createEmbeddings } from "../../services/createEmbeddings";
+import { TextEmbedding } from '../../types/file'
+import extractTextFromFile from '../../services/extractTextFromFile'
+import { createEmbeddings } from '../../services/createEmbeddings'
 
 // Disable the default body parser to handle file uploads
-export const config = { api: { bodyParser: false } };
+export const config = { api: { bodyParser: false } }
 
 type Data = {
-  text?: string;
-  meanEmbedding?: number[];
-  chunks?: TextEmbedding[];
-  error?: string;
-};
+  text?: string
+  meanEmbedding?: number[]
+  chunks?: TextEmbedding[]
+  error?: string
+}
 
 // This function receives a file as a multipart form and returns the text extracted fom the file and the OpenAI embedding for that text
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  if (req.method !== "POST") {
-    res.status(405).json({ error: "Method not allowed" });
-    return;
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  if (req.method !== 'POST') {
+    res.status(405).json({ error: 'Method not allowed' })
+    return
   }
 
   // Create a formidable instance to parse the request as a multipart form
-  const form = new formidable.IncomingForm();
-  form.maxFileSize = 30 * 1024 * 1024; // Set the max file size to 30MB
+  const form = new formidable.IncomingForm()
+  // form.maxFileSize = 30 * 1024 * 1024; // Set the max file size to 30MB
 
   try {
     const { fields, files } = await new Promise<{
-      fields: Fields;
-      files: Files;
+      fields: Fields
+      files: Files
     }>((resolve, reject) => {
       form.parse(req, (err, fields, files) => {
         if (err) {
-          reject(err);
+          reject(err)
         } else {
-          resolve({ fields, files } as { fields: Fields; files: Files });
+          resolve({ fields, files } as { fields: Fields; files: Files })
         }
-      });
-    });
-    const file = files.file;
+      })
+    })
+    const file = files.file
     if (!file || Array.isArray(file) || file.size === 0) {
-      res.status(400).json({ error: "Invalid or missing file" });
-      return;
+      res.status(400).json({ error: 'Invalid or missing file' })
+      return
     }
 
     const text = await extractTextFromFile({
       filepath: file.filepath,
-      filetype: file.mimetype ?? "",
-    });
+      filetype: file.mimetype ?? ''
+    })
 
     const { meanEmbedding, chunks } = await createEmbeddings({
-      text,
-    });
+      text
+    })
 
-    res.status(200).json({ text, meanEmbedding, chunks });
+    res.status(200).json({ text, meanEmbedding, chunks })
   } catch (error: any) {
-    res.status(500).json({ error: error.message });
+    res.status(500).json({ error: error.message })
   } finally {
     // Always send a response, even if it is empty
-    res.end();
+    res.end()
   }
 }
